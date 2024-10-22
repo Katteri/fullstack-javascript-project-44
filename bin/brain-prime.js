8220a6124c508c5f535d34362f72a6ed5e77fb7c
Ekaterina Domashova <katy.domashova@gmail.com>
2024-10-22 18:26:14 +0300

    brain-prime game

---
+++ bin/brain-prime.js
@@ -0,0 +1,35 @@
+#!/usr/bin/env node
+import readlineSync from 'readline-sync';
+
+function prime(num) {
+  for (let i = 2; i < Math.sqrt(num); i += 1) {
+    if (num % i === 0) {
+      return false;
+    }
+  }
+  return true;
+}
+
+console.log('Welcome to the Brain Games!');
+const name = readlineSync.question('May I have your name? ');
+console.log(`Hello, ${name}!`);
+console.log('Answer "yes" if given number is prime. Otherwise answer "no".');
+
+let count = 0;
+while (count < 3) {
+  const number1 = Math.floor(Math.random() * 100);
+  const right = prime(number1) ? 'yes' : 'no';
+
+  console.log(`Question: ${number1}`);
+  const answer = readlineSync.question('Your answer: ');
+
+  if (right === answer) {
+    console.log('Correct!');
+    count += 1;
+  } else {
+    console.log(`'${answer}' is wrong answer ;(. Correct answer was '${right}'.`);
+    console.log(`Let's try again, ${name}!`);
+  }
+}
+
+console.log(`Congratulations, ${name}!`);
